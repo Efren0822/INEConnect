e5361ebccae6448ad08355a7e66cf4487f403b26
Efren <practicas038@gmail.com>
2024-03-20 13:37:19 -0600

    Se modifico el login, ahora cuando se inicia sesion se redirije al usuario a otra vista

--- app/src/main/java/com/ineconnect/Componentes/Bienvenida.kt
+++ app/src/main/java/com/ineconnect/Componentes/Bienvenida.kt
@@ -1,2 +1,9 @@
 package com.ineconnect.Componentes
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun bienvenida(){
+    Text(text = "Bienvenido")
+}
--- app/src/main/java/com/ineconnect/MainActivity.kt
+++ app/src/main/java/com/ineconnect/MainActivity.kt
@@ -1,29 +1,36 @@
 package com.ineconnect
 
 import android.os.Bundle
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
 import com.ineconnect.Componentes.Login
 import com.ineconnect.ui.theme.INEConnectTheme
+import androidx.navigation.compose.*
+import com.ineconnect.Componentes.bienvenida
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             INEConnectTheme {
-                // A surface container using the 'background' color from the theme
+                val navController = rememberNavController()
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MainContent()
+                    MainContent(navController)
                 }
             }
         }
@@ -31,16 +38,26 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MainContent() {
-    Login(
-        onLoginClick = { username, password ->
-            println("Inicio de sesión exitoso")
-            println("Username: $username, Password: $password")
-        },
-        onRegisterClick = {
+fun MainContent(navController: NavHostController) {
+    NavHost(navController, startDestination = "login") {
+        composable("login") {
+            Login(
+                onLoginClick = { username, password ->
+                    println("Inicio de sesión exitoso")
+                    println("Username: $username, Password: $password")
+                },
+                onRegisterClick = {
+                    println("Abrir componente de registro")
+                },
+                onSuccessRedirect = { username ->navController.navigate("bienvenida/$username")
 
-            println("Abrir componente de registro")
-        },
-
-    )
+                }
+            )
+        }
+        composable("bienvenida/{username}") { // Definir la ruta para la Bienvenida
+            // Aquí se puede definir el componente de Bienvenida
+            // Por ejemplo, podrías llamar a una función que representa este componente
+            bienvenida()
+        }
+    }
 }
